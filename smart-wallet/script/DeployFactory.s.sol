c35383b86a728d3fe0e2dc5a096e2fbcdf5973d2
Your Name <chester.kuo@plusblocks.io>
2025-04-25 16:48:26 +0000

    update

---
+++ smart-wallet/script/DeployFactory.s.sol
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Script, console2} from "forge-std/Script.sol";
+import {SafeSingletonDeployer} from "safe-singleton-deployer-sol/src/SafeSingletonDeployer.sol";
+
+import {CoinbaseSmartWallet, CoinbaseSmartWalletFactory} from "../src/CoinbaseSmartWalletFactory.sol";
+
+contract DeployFactoryScript is Script {
+    // address constant EXPECTED_IMPLEMENTATION = 0x000100abaad02f1cfC8Bbe32bD5a564817339E72;
+    // address constant EXPECTED_FACTORY = 0x0BA5ED0c6AA8c49038F819E587E2633c4A9F428a;
+    address constant EXPECTED_FACTORY = 0x24F3118A61E2e728fA6b29C69dEC67dA0DA29840;
+    address constant EXPECTED_IMPLEMENTATION = 0x1723d60814f203FdF3fCc4dD5Bc8C183e56Ef1D4;
+
+    function run() public {
+        console2.log("Deploying on chain ID", block.chainid);
+        address implementation = SafeSingletonDeployer.broadcastDeploy({
+            creationCode: type(CoinbaseSmartWallet).creationCode,
+            //salt: 0x3438ae5ce1ff7750c1e09c4b28e2a04525da412f91561eb5b57729977f591fbb
+            salt: 0x2438ae5ce1ff7750c1e09c4b28e2a04525da412f91561eb5b57729977f591fbb
+        });
+        console2.log("implementation", implementation);
+        // assert(implementation == EXPECTED_IMPLEMENTATION);
+        address factory = SafeSingletonDeployer.broadcastDeploy({
+            creationCode: type(CoinbaseSmartWalletFactory).creationCode,
+            args: abi.encode(EXPECTED_IMPLEMENTATION),
+            //salt: 0x278d06dab87f67bb2d83470a70c8975a2c99872f290058fb43bcc47da5f0390c
+            salt: 0x178d06dab87f67bb2d83470a70c8975a2c99872f290058fb43bcc47da5f0390c
+        });
+        console2.log("factory", factory);
+        // assert(factory == EXPECTED_FACTORY);
+    }
+}
